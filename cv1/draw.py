c434a8efc8d9e86d58b091d3627cec309693f591
bayertom <bayertom@natur.cuni.cz>
2025-02-20 10:46:20 +0100

    cv1

---
+++ cv1/draw.py
@@ -0,0 +1,12 @@
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtGui import QMouseEvent, QPaintEvent
+from PyQt6.QtWidgets import *
+
+
+class Draw(QWidget):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+     
+       
