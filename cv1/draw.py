9daad9c659b6870dbed248d46cda338e9215d10d
bayertom <bayertom@natur.cuni.cz>
2025-02-20 18:33:56 +0100

    cv1_ok

--- cv1/draw.py
+++ cv1/draw.py
@@ -3,15 +3,14 @@ from PyQt6.QtGui import *
 from PyQt6.QtGui import QMouseEvent, QPaintEvent
 from PyQt6.QtWidgets import *
 
-
 class Draw(QWidget):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__q = QPointF(0,0)
+        self.__q = QPointF(0.0, 0.0)
         self.__pol = QPolygonF()
         
-        
+
     def mousePressEvent(self, e):
         
         #Get coordinates x,y
@@ -21,3 +20,5 @@ class Draw(QWidget):
         #Add to point to polygon
         
         #Repaint screen
+        
+        pass
