3ae0b06d6174b403ce233bd87fb5057f95cc290b
bayertom <bayertom@users.noreply.github.com>
2025-02-20 14:36:01 +0100

    cv1

--- cv1/draw.py
+++ cv1/draw.py
@@ -8,5 +8,16 @@ class Draw(QWidget):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-     
-       
+        self.__q = QPointF(0,0)
+        self.__pol = QPolygonF()
+        
+        
+    def mousePressEvent(self, e):
+        
+        #Get coordinates x,y
+        
+        #Create new point
+        
+        #Add to point to polygon
+        
+        #Repaint screen
