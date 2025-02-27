168d760388c988bb5aacd4cc026c01f67130b783
bayertom <bayertom@users.noreply.github.com>
2025-02-27 13:08:13 +0100

    cv2

--- cv1/draw.py
+++ cv1/draw.py
@@ -11,14 +11,35 @@ class Draw(QWidget):
         self.__pol = QPolygonF()
         
 
-    def mousePressEvent(self, e):
+    def mousePressEvent(self, e:QMouseEvent):
         
         #Get coordinates x,y
+        x = e.position().x()
+        y = e.position().y()
         
         #Create new point
+        p = QPointF(x, y)
         
         #Add to point to polygon
+        self.__pol.append(p)
         
         #Repaint screen
+        self.repaint()
+ 
+ 
+    def paintEvent(self, e: QPaintEvent):
+        #Create new graphic object
+        qp = QPainter(self)
         
-        pass 
+        #Start draw
+        qp.begin(self)
+      
+        #Set graphic attributes
+        qp.setPen(Qt.GlobalColor.black)
+        qp.setBrush(Qt.GlobalColor.yellow)
+        
+        #Draw objects
+        qp.drawPolygon(self.__pol)
+        
+        #End drawing
+        qp.end()
